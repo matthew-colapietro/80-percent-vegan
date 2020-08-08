f8d7c5245b361635f45b87b558d4d861c5633547
MCola <mcolapietro502@gmail.com>
2020-08-08 16:56:08 -0400

    added redux via npm; began setting up redux promise

--- Post-Program/80-percent-vegan/src/components/Dairy.js
+++ Post-Program/80-percent-vegan/src/components/Dairy.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 
 class Dairy extends Component {
+  
+
 
   render () {
     return (
       <div>
-        <p>This is the dairy section</p>
+        <p>Milk/Cream might be the easiest and most cost effective healthy replacement. There are dozens of options for milk and cream which have only improved as their popularity has soared.</p>
+
       </div>
     )
   }
---
+++ Post-Program/80-percent-vegan/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+//the result of the reducer data goes to the "", "trash", "event" key
+const rootReducer = combineReducers({
+  // dairy: DairyReducer,
+});
+
+export default rootReducer;
