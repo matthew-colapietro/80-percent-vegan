183cd635d1d1ced39d1b17f471643c227809883c
MCola <mcolapietro502@gmail.com>
2020-08-07 20:56:59 -0400

    added App componenet

---
+++ Post-Program/80-percent-vegan/src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        80% Vegan
+      </div>
+    )
+  }
+}
+
+export default App;
